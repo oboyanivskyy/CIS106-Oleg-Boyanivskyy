7f7f24046a30890430dfc6bc670c400c9dadaeb8
oboyanivskyy <77712096+oboyanivskyy@users.noreply.github.com>
2021-02-01 16:24:43 -0600

    Add files via upload

---
+++ Activity 7.py
@@ -0,0 +1,7 @@
+# This program will calculate the age of your dog in dog years.
+print("What is your dog's name?")
+name = input()
+print("How old is your dog in human years?")
+years = float(input())
+total = years * 7
+print(name + " is " + str(total) + " years old in dog years")
