9332254263ec9c26e415fd09b1ede7f5edfe0225
oboyanivskyy <77712096+oboyanivskyy@users.noreply.github.com>
2021-02-08 20:03:25 -0600

    Add files via upload

---
+++ Assignment 4/Chapter Activity 2.py
@@ -0,0 +1,7 @@
+#This program will convert miles into yards, feet, and inches
+print("Enter # of miles")
+miles = float(input())
+yards = miles * 1760
+feet = miles * 5280
+inches = miles * 63360
+print(" The total distance is " + str(miles) + " miles, " + str(yards) + " yards, " + str(feet) + " feet, and " + str(inches) + " inches. ")
