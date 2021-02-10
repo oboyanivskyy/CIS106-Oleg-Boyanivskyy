f91e1f59e36258a86960994ed527c1e2a2405554
oboyanivskyy <77712096+oboyanivskyy@users.noreply.github.com>
2021-02-09 21:13:49 -0600

    Update Activity 2.py

--- Assignment 4/Activity 2.py
+++ Assignment 4/Activity 2.py
@@ -9,6 +9,4 @@ days = years * 365
 hours = days * 24
 seconds = hours * 3600
 
-print("You are " + str(years) + " years, " + str(months) + " months, ")
-print(+ str(days) + " days, " + str(hours) + " hours, " + str(seconds) + " seconds old ")
-
+print("You are " + str(months) + " months," + str(days) + " days, " + str(hours) + " hours, " + str(seconds) + " seconds old ")
