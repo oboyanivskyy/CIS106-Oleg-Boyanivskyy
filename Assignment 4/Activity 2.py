ff8f96053cb43b3d7b2b93a8099c0a24b3c57dbd
oboyanivskyy <77712096+oboyanivskyy@users.noreply.github.com>
2021-02-09 20:54:49 -0600

    Update Activity 2.py

--- Assignment 4/Activity 2.py
+++ Assignment 4/Activity 2.py
@@ -1,11 +1,15 @@
-#This program will display your age in months, days, hours, and seconds.
+# This program will display your age in months, days, hours, and seconds.
 
 print("Enter your age in years")
 
 years = float(input())
+
 months = years * 12
+
 days = years * 365
+
 hours = days * 24
+
 seconds = hours * 3600
 
 print("You are " + str(years) + " years, " + str(months) + " months, " + str(days) + " days, " + str(hours) + " hours, " + str(seconds) + " seconds old ")
