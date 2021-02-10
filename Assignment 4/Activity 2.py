0aa6fb02e9921ef4edf6c2a755878b1cbbef6551
oboyanivskyy <77712096+oboyanivskyy@users.noreply.github.com>
2021-02-09 21:25:33 -0600

    Update Activity 2.py

--- Assignment 4/Activity 2.py
+++ Assignment 4/Activity 2.py
@@ -5,8 +5,11 @@ print("Enter your age in years")
 years = float(input())
 
 months = years * 12
+
 days = years * 365
+
 hours = days * 24
+
 seconds = hours * 3600
 
-print("You are " + str(months) + " months," + str(days) + " days, " + str(hours) + " hours, " + str(seconds) + " seconds old ")
+print("You are " + str(months) + " months " + str(days) + " days " + str(hours) + " hours " + str(seconds) + " seconds old ")
