930fd1e668666291f1c24420174cc18899b5ed10
Dave Braunschweig <dbraunsc@harpercollege.edu>
2021-02-10 07:35:37 -0600

    Assignment 4 corrections

--- Assignment 4/Activity 2.py
+++ Assignment 4/Activity 2.py
@@ -1,21 +1,14 @@
 # This program will display your age in months, days, hours, and seconds.
 
 print("Enter your age in years")
-
 years = float(input())
 
 months = years * 12
-
 days = years * 365
-
 hours = days * 24
-
-seconds = hours * 3600
+seconds = hours * 60 * 60
 
 print("You are " + str(months) + " months ")
-
 print(str(days) + " days ")
-
 print(str(hours) + " hours ")
-
 print(str(seconds) + " seconds old ")
--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -1,19 +1,13 @@
 # This program will convert miles into yards, feet, and inches
 
 print("Enter # of miles")
-
 miles = float(input())
 
 yards = miles * 1760
-
 feet = miles * 5280
-
 inches = miles * 63360
 
 print(" The total distance is ")
-
 print(str(yards) + " yards, ")
-
 print(str(feet) + " feet, and ")
-
 print(str(inches) + " inches. ")
