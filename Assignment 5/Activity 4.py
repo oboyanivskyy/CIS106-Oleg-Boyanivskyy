b91c51e8a69034633eeec5320aa506b621563c2c
oboyanivskyy <77712096+oboyanivskyy@users.noreply.github.com>
2021-02-16 20:49:28 -0600

    Modified program to simplify output and processing

--- Assignment 5/Activity 4.py
+++ Assignment 5/Activity 4.py
@@ -1,38 +1,38 @@
-# This program will calculate area for a triangle, rectangle, and parallelogram
-
-def getTriangle():
-    print("Enter the base of the triangle")
+def calculatePara():
+    print("What is the base of the parallelogram?")
     base = int(input())
-    print("Enter height of the triangle")
-    height = int(input())
-    triangle = 0.5 * base * height
-    
-    return triangle
-
-def getRectangle():
-    print("Enter the height of the rectangle")
-    height = int(input())
-    print("Enter the width of the rectangle.")
-    width = int(input())
-    rectangle = width * height
-    
-    return rectangle
-
-def getParallel():
-    print("Enter the base of the parallelogram")
-    base = int(input())
-    print("Enter the vertical height of the parallelogram")
+    print("What is the height of the parallelogram?")
     height = int(input())
     parallel = base * height
     
     return parallel
 
+def calculateRect():
+    print("What is the width of the rectangle?")
+    width = float(input())
+    print("What is the height of the rectangle?")
+    height = float(input())
+    rectangle = width * height
+    
+    return rectangle
+
+def calculateTrian():
+    print("What is the base of the triangle?")
+    base = int(input())
+    print("What is the height of the triangle?")
+    height = int(input())
+    triangle = float(1) / 2 * base * height
+    
+    return triangle
+
+def displayArea(rectangle, triangle, parallel):
+    print("The area of the rectangle is " + str(rectangle))
+    print("The area of the triangle is " + str(triangle))
+    print("The area of the parallelogram is " + str(parallel))
+
 # Main
-
-triangle = getTriangle()
-rectangle = getRectangle()
-parallel = getParallel()
-
-print("Area of triangle = " + str(triangle))
-print("Area of rectangle = " + str(rectangle))
-print("Area of parallelogram = " + str(parallel))
+# This program will calculate area for a triangle, rectangle, and parallelogram
+rectangle = calculateRect()
+triangle = calculateTrian()
+parallel = calculatePara()
+displayArea(rectangle, triangle, parallel)
