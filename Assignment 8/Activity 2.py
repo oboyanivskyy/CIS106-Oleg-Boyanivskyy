83c9d649395c04366a1a4d7ca17f474c90ee6dc9
oboyanivskyy <77712096+oboyanivskyy@users.noreply.github.com>
2021-03-09 10:13:27 -0600

    Accidently uploaded wrong file

--- Assignment 8/Activity 2.py
+++ Assignment 8/Activity 2.py
@@ -1,51 +1,38 @@
-# This updated program asks the user for their hours per week,
-# and rate, then calculates weekly, monthly, and annual pay,
-# calculates overtime, and displays the result.
+# This program will take your grade scores,
+# add it to a total, calculate, and display,
+# the average for the entered scores.
 
+def get_amount():
+    print("How many scores do you want to enter?")
+    amount = int(input())
     
-def get_hours():
-    print("Enter Hours Worked Per Week")
-    hours = float(input())
-    return hours
+    return amount
 
-
-def get_rate():
-    print("Enter Your Rate Per Hour")
-    rate = float(input())
-    return rate
-
-
-def calculate_weekly(rate, hours):
-    if hours > 40:
-        weekly = (rate * hours) + (hours - 40) * (0.5 * rate)
-    else:
-        weekly = rate * hours
-    return weekly
-
-
-def calculate_monthly(weekly):
-    monthly = weekly * 4
-    return monthly
-
-
-def calculate_annual(weekly):
-    annual = weekly * 52
-    return annual
-
-
-def display_result(weekly, monthly, annual):
-    print("Your Weekly Pay is $" + str(weekly))
-    print(" Monthly Pay is $" + str(monthly))
-    print(" Yearly Pay is $" + str(annual))
+def get_score(amount):
+    count = 1
+    totalscore = 0
+    while count <= amount:
+        print("Enter score " + str(count))
+        score = int(input())
+        count = count + 1
+        totalscore = totalscore + score
     
+    return totalscore
+
+def calculate_average(amount,totalscore):
+    total = totalscore/amount
+    
+    return total
+
+def display_average(total):
+    print("Your average for entered scores is ")
+    print(str(total))
     
 def main():
-    hours = get_hours()       
-    rate = get_rate()
-    weekly = calculate_weekly(rate, hours)
-    monthly = calculate_monthly(weekly)
-    annual = calculate_annual(weekly)
-    display_result(weekly, monthly, annual)
-
-
-main()                
+    amount = get_amount()
+    totalscore = get_score(amount)
+    total = calculate_average(amount,totalscore)
+    display_average(total)
+    
+    
+main()
