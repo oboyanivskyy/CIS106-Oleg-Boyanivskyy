cfea7d321f15c9e368f84d803a3327f672976efa
Dave Braunschweig <dbraunsc@harpercollege.edu>
2021-04-14 17:56:21 -0500

    Assignment 12 corrections

--- Assignment 12/Activity 2.py
+++ Assignment 12/Activity 2.py
@@ -4,23 +4,19 @@
 
 def calculate_number():
     array = []
-    start = 50
     start_range = 0
     end_range = 100
     while True:
+        start = round((start_range + end_range) / 2)
+        array.append(start)
         response = get_response(start)
         if response == str("e"):
-            array.append(start)
             display_result(array, response)
             break
         elif response == str("h"):
-            array.append(start)
             start_range = start
-            start = round((start_range + end_range) / 2)
         elif response == str("l"):
-            array.append(start)
             end_range = (end_range / 2)
-            start = round((start_range + end_range) / 2)
         else:
             display_error()
         
