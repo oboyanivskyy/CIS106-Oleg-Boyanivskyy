4ed9c2f64e2fcdccb7f2640698f9fa01d2ebc638
oboyanivskyy <77712096+oboyanivskyy@users.noreply.github.com>
2021-04-14 15:09:16 -0500

    Add files via upload

--- Assignment 12/Activity 2.py
+++ Assignment 12/Activity 2.py
@@ -10,19 +10,19 @@ def calculate_number():
     while True:
         response = get_response(start)
         if response == str("e"):
-            array.append(response)
+            array.append(start)
             display_result(array, response)
             break
         elif response == str("h"):
-            array.append(response)
+            array.append(start)
             start_range = start
             start = round((start_range + end_range) / 2)
         elif response == str("l"):
-            array.append(response)
+            array.append(start)
             end_range = (end_range / 2)
             start = round((start_range + end_range) / 2)
         else:
-            print("Invalid Response")
+            display_error()
         
 
 def get_response(start):
@@ -31,6 +31,10 @@ def get_response(start):
     
     return response
 
+
+def display_error():
+    print("Invalid Response")
+    
     
 def display_result(array, response):
     print("I guessed your number!")
