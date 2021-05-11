ceebbb31fb97e9aa381432169bd350d8d7599558
oboyanivskyy <77712096+oboyanivskyy@users.noreply.github.com>
2021-05-10 20:42:08 -0500

    Add files via upload

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -3,14 +3,60 @@
 # display the menu items.
 
 
-def get_title(title, line):
-    
+def get_title(title, line):    
     if line.find("TITLE") == True:
-        line.replace("<TITLE>", "")
-        line.replace("</TITLE>", "")
+        line = line.replace("<TITLE>", "")
+        line = line.replace("</TITLE>", "")
         title.append(line)
     else:
         pass
+
+
+def get_artist(artist, line):    
+    if line.find("ARTIST") == True:
+        line = line.replace("<ARTIST>", "")
+        line = line.replace("</ARTIST", "")
+        artist.append(line)
+    else:
+        pass
+
+
+def get_country(country, line):    
+    if line.find("COUNTRY") == True:
+        line = line.replace("<COUNTRY>", "")
+        line = line.replace("</COUNTRY>", "")
+        country.append(line)
+    else:
+        pass
+    
+
+def get_company(company, line):    
+    if line.find("COMPANY") == True:
+        line = line.replace("<COMPANY>", "")
+        line = line.replace("</COMPANY>", "")
+        company.append(line)
+    else:
+        pass
+    
+    
+def get_price(price, line):    
+    if line.find("PRICE") == True:
+        line = line.replace("<PRICE>", "")
+        line = line.replace("</PRICE>", "")
+        price.append(line)
+    else:
+        pass
+
+
+def get_year(year, line):    
+    if line.find("YEAR") == True:
+        line = line.replace("<YEAR>", "")
+        line = line.replace("</YEAR>", "")
+        year.append(line)
+    else:
+        pass
+    
+    
 def read_data(filename):
     title = []
     artist = []
@@ -22,12 +68,12 @@ def read_data(filename):
         for line in file:
             line = line.strip()
             get_title(title, line)
-#            get_artist(artist, line)
-#            get_country(country, line)           
-#            get_company(company, line)            
-#            get_price(price, line)
-#            get_year(year, line)
-        print(title)
+            get_artist(artist, line)
+            get_country(country, line)           
+            get_company(company, line)            
+            get_price(price, line)
+            get_year(year, line)
+
             
 def main():
     filename = "cd_catalog.xml"
